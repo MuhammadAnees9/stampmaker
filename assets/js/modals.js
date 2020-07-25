e11890a77232eabaa45220c12d4856402929dc73
MusawerAli <pakjalihouse@gmail.com>
2020-07-25 10:15:56 -0500

    fix bug

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -102,7 +102,7 @@ function ajaxCall() {
 
 function logout() {
   $.ajax({
-    url: "https://test.mystampmaker.com/logout.php", //the page containing php script
+    url: base_url + "logout.php", //the page containing php script
     type: "post", //request type,
     dataType: 'json',
     success: function (response) {
