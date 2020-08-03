90d5fb0a93ae1291d00e4f206732517cd323105e
MusawerAli <pakjalihouse@gmail.com>
2020-08-03 15:59:02 -0500

    test

--- assets/js/valid.js
+++ assets/js/valid.js
@@ -1,48 +1,43 @@
 $(document).ready(function () {
-    setTimeout(function () {
-        valid();
-    }, 1000);
+  setTimeout(function () {
+    valid();
+  }, 1000);
 });
 
-
 function valid() {
-    $('.dataTables_filter input').val('');
-    $('.dataTables_filter input').attr({
-        'autocomplete': 'off'
-    });
-    $('.pwd').val('');
+  $(".dataTables_filter input").val("");
+  $(".dataTables_filter input").attr({
+    autocomplete: "off",
+  });
+  $(".pwd").val("");
 
-    $('#pwd').val('');
+  $("#pwd").val("");
 
-    $('#email').val('');
+  $("#email").val("");
 
-    $('#usernametext').val('');
+  $("#usernametext").val("");
 
-    $('.pwd').attr({
-        'autocomplete': 'off'
-    });
+  $(".pwd").attr({
+    autocomplete: "off",
+  });
+  $(".pwd").attr({
+    autocomplete: "off",
+  });
+  $(".password").attr({
+    autocomplete: "off",
+  });
 
-    $('.pwd').attr({
-        'autocomplete': 'off'
-    });
+  $("#usernametext").attr({
+    autocomplete: "off",
+  });
 
-    $('.password').attr({
-        'autocomplete': 'off'
-    });
+  $(".password").val("");
 
-    $('#usernametext').attr({
-        'autocomplete': 'off'
-    });
+  $("#email").attr({
+    autocomplete: "off",
+  });
 
-    $('.password').val('');
-
-    $('#email').attr({
-        'autocomplete': 'off'
-    });
-
-    $('#pass').attr({
-        'autocomplete': 'off'
-    });
-
-
+  $("#pass").attr({
+    autocomplete: "off",
+  });
 }
