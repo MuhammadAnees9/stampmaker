03b774b7b57f5634e904aabb3e8e0bfb1230712d
Mudassar755 <mudasar.se@gmail.com>
2020-11-12 21:13:01 +0500

    empty-branch

--- register.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "dbConfig.php";
-$id = $_GET["id"];
-$sql = "update user set isActive = 1 where id = '$id'";
-$conn->query($sql);
-echo "<script>window.location.href = 'index.php';</script>";
-?>
