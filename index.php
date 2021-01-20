e30403d5a1dab577bcdf90ece0276f88d3293141
softwareeng-web <77640365+softwareeng-web@users.noreply.github.com>
2021-01-21 03:04:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -50,6 +50,28 @@ include_once('header.php');
 <div class="container">
     <!-- Scrolling Wrapper End-->
     <div class="row">
+	<ul class="col-lg-2">
+		<button class="btn o" id="addroundtext" title="Text Around The Circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px"
+                        height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <path fill="#ffffff"
+                            d="M136 153l-37 0 -10 23 -27 0 49 -102 27 0 27 55c-12,5 -22,13 -29,24zm5 -17l-17 -38 -17 38 34 0zm-21 -126c-61,0 -110,50 -110,111 0,61 49,110 110,110 7,0 13,0 20,-2 -7,-6 -12,-14 -16,-23 -1,0 -2,0 -4,0 -47,0 -85,-38 -85,-85 0,-47 38,-85 85,-85 47,0 85,38 85,85 0,1 0,1 0,2 9,3 18,8 24,15 1,-6 2,-12 2,-17 0,-61 -50,-111 -111,-111zm60 140l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -28 0 -2zm2 -15c-30,4 -50,30 -46,58 3,27 27,50 58,46 16,-2 27,-10 34,-18 19,-23 15,-57 -6,-74 -11,-9 -24,-14 -40,-12z">
+                        </path>
+                    </svg>
+
+                    <br><span style="color:white;">Round Text</span>
+                </button>
+                <button class="btn o" id="addlinetext" title="Line Text">
+                    <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px"
+                        height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <path fill="#ffffff"
+                            d="M5 236l60 1 15 -38 45 0c-1,-20 3,-35 15,-50 1,-1 1,-2 2,-2l-41 -1 24 -59 22 54c1,0 7,-5 10,-7 13,-8 27,-11 43,-10l-45 -113 -60 0 -90 225zm180 -82l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -27 0 -3zm2 -15c-30,4 -50,30 -47,58 3,27 28,50 58,47 16,-2 28,-10 35,-19 19,-22 15,-56 -6,-74 -11,-8 -25,-14 -40,-12z">
+                        </path>
+                    </svg>
+                    <br><span style="color:white;">Line Text</span>
+
+                </button>
+	</ul>
         <ul class="bookmarks col-lg-2 col-sm-2 col-md-3 col-2" id="labels">
         </ul>
         <div class="col-lg-6 canvas col-sm-3 col-10 col-md-4" style="margin-top:40px;">
