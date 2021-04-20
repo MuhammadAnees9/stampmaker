a17ac18b31388c51991e4de186100e2a888d6a67
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-04-20 16:41:37 -0700

    mail checking

--- inc/suggest.php
+++ inc/suggest.php
@@ -8,7 +8,7 @@ require_once "PHPMailer/PHPMailer.php";
 require_once "PHPMailer/SMTP.php";
 require_once "PHPMailer/Exception.php";
 $text = $_POST["text"];
-$to = "stampmakerapp@gmail.com";
+$to = "muhammadusman78@yandex.com";
 $from = $_SESSION["email"];
 $id = $_SESSION["id"];
 $subject = "Concern Or Suggestion";
