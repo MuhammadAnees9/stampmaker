50c7215c71e1db991a26d7413eb4a232be84cc2d
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-04-19 18:54:28 -0700

    suggestion changes

--- inc/suggestion.php
+++ inc/suggestion.php
@@ -7,18 +7,19 @@ if (empty($_SESSION["uid"])) {
 function getSuggest()
 {
     echo "
-          
-          <div class='suggest bottomright' id='suggest'>
-          <center><h5>Leave Suggestion</h5></center>
-          <textarea type='text' class='signup form-control' id='suggestText' placeholder='Enter your concerns or suggestions here.' required=''></textarea>
-          <br><center><button class='modals btn btn-md btn-success' onclick='suggestion()'>Send</button></center>
-        </div>
-        
-<div class='demo bottomright' onclick='togglesuggestions()'>
-To send concerns or suggestions.<b>Click Here</b>
-</div>
-
-
-
-";
+        <div class='demo bottomright seggestions-section'> 
+        	<div id='suggestionbox' onclick='togglesuggestions()'>
+	            To send concerns or suggestions.<b>Click Here</b>
+	        </div>
+	        <div class='suggest' id='suggest'>
+	        	<div class='card'>
+                    <div class='card-body'>
+				        <center><h5>Leave Suggestion</h5></center>
+				        <textarea type='text' class='signup form-control' id='suggestText' placeholder='Enter your concerns or suggestions here.' required=''></textarea>
+				        <br><center><button class='modals btn btn-md btn-success' onclick='suggestion()'>Send</button></center>
+				    </div>
+				</div>        
+	        </div>
+	    </div>
+    ";
 } ?>
