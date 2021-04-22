036f0a6de831b53dfc5f01e6f6a727097b0d25b4
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-04-22 10:11:04 -0700

    restore suggestion mail

--- inc/suggestionmail.php
+++ inc/suggestionmail.php
@@ -1,18 +1,61 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>PHP</h1>
-  
-<?php 
-$text = $_POST["text"];
-echo "my" . $text . "here"; 
-?>
-  
 <?php
-echo "Hello World!";
-?>
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-</body>
-</html>
+use PHPMailer\PHPMailer\PHPMailer;
+include "dbConfig.php";
+require_once "../PHPMailer/PHPMailer.php";
+require_once "../PHPMailer/SMTP.php";
+require_once "../PHPMailer/Exception.php";
+$text = $_POST["text"];
+$to = "muhammadusman78@yandex.com";
+$from = $_SESSION["email"];
+$id = $_SESSION["id"];
+$subject = "Concern Or Suggestion";
+
+
+
+$recipient = $to; // this is receipient email address.
+
+$usernameSmtp = 'stampmakerapp@gmail.com';   // Remember to Change: this is your gmail adddress.
+$passwordSmtp = 'ppasoxlcdihmznry';            // This is your gmail password
+
+$mail = new PHPMailer(true);
+
+try {
+    // Specify the SMTP settings.
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+$mail->SMTPSecure = 'tls';
+$mail->SMTPKeepAlive = true;
+$mail->Mailer = "smtp";
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->Username = $usernameSmtp;/*SMTP username*/
+$mail->Password = $passwordSmtp;/*SMTP password*/
+$mail->SMTPAuth = true;/*Enable SMTP authentication*/
+$mail->SMTPDebug = 0;//set debug
+$mail->From = $usernameSmtp;// from address to which mail is sent
+$mail->FromName = 'Stamp Maker (NO REPLY)';// specifying sender name
+$mail->AddAddress($recipient);// specifying to address
+$mail->AddReplyTo($usernameSmtp, 'Information');// specifying the mail address to which replay is sent
+$mail->IsHTML(true);
+$mail->Subject = $subject;
+$mail->AltBody = "To view the message, please use an HTML compatible email viewer!";
+$message .= "<b>From:</b> ".$from."<br><b>User ID:</b> ".$id."<br>";
+
+$message .= "<b>Message:</b> ".$text;
+$mail->Body = $message;
+    $mail->Send();
+    echo json_encode(array("abc"=>'done',"msg" => "Email sent!"));;
+} catch (phpmailerException $e) {
+    echo json_encode(array("abc"=>'not-done',"msg" => "An error occurred. {$e->errorMessage()}"));
+     //Catch errors from PHPMailer.
+} catch (Exception $e) {
+     echo json_encode(array("abc"=>'not-done',"msg" => "Email not sent. {$mail->ErrorInfo}"));
+}
+
+
+?>
 
