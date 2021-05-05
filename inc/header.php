d3a3150b388f159488d1e2b449727f8074aa7545
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-05-05 13:54:09 -0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -15,24 +15,20 @@ $base_url = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER["REQUEST_URI"
 if (isset($_SESSION['uid']) && $_SESSION['role'] == 'admin') {
     // header( 'Location:admin');
 }
+
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <link rel="icon" type="image/png" href="logofav.png" sizes="32x32">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171780865-1"></script>
     <script src="assets/js/google_anylatic.js">
     </script>
     <script src="assets/js/jquery-3.5.1.js"></script>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <script src="assets/js/bootstrap.min.js"></script>
-    <script src="http://hongru.github.io/proj/canvas2image/canvas2image.js"></script>
-
-    <script type="text/javascript" src="assets/js/customCircularLibrary.js"></script>
-    <!-- <link rel="stylesheet" type="text/css" href="assets/css/style.css"> -->
-    <link rel="stylesheet" type="text/css" href="assets/css/stampMakerStyle.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -41,7 +37,15 @@ if (isset($_SESSION['uid']) && $_SESSION['role'] == 'admin') {
     <script type="text/javascript" src="assets/js/modals.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="assets/js/select2.min.js"></script>
+    <script src="assets/js/cans.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/konva/7.2.5/konva.js"></script>
     <link href="assets/css/select2.min.css" rel="stylesheet" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <script src="http://hongru.github.io/proj/canvas2image/canvas2image.js"></script>
+
+    <script type="text/javascript" src="assets/js/customCircularLibrary.js"></script>
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="assets/css/stampMakerStyle.css?v=<?php echo time(); ?>">
     <title>Stamp Maker</title>
 </head>
 
