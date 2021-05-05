9a146f676c2ecafe95c5ac2039653077161ebf31
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-05-05 13:49:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,336 @@
 <?php
-session_start();
+//session_start();
 
 include_once('inc/header.php');
 
 ?>
 
+<style type="text/css">
+  @media screen and (max-width: 1600px) {
+      #properties{
+        -ms-flex: 0 0 23%;
+        flex: 0 0 23%;
+        max-width: 23%;
+      }
+  } 
+
+  @media screen and (max-width: 1600px) {
+      #properties{
+        -ms-flex: 0 0 21%;
+        flex: 0 0 21%;
+        max-width: 21%;
+      }
+
+      .btns{
+        width: 7.9em;
+      }
+  } 
+
+  @media screen and (max-width: 768px) {
+  .wrapper {
+    margin-top: 6em;
+  }
+  .canvas-col {
+    margin-top: 1.5em;
+    -ms-flex: 0 0 50%;
+    flex: 0 0 43%;
+    max-width: 43%;
+  }
+  .canvas-col .col-md-8{
+    max-width: 100%;
+    flex: 0 0 100%;
+  }
+  #properties {
+    -ms-flex: 0 0 35%;
+    flex: 0 0 30%;
+    max-width: 30%;
+  }
+  #labels-container {
+    margin-top: 1.5em;
+  }
+  .desktop-view {
+    display: none;
+  }
+  .mobile-view {
+    display: block;
+    position: relative;
+    left: 0px;
+    bottom: 0.8em;
+  }
+  .left-banner, .right-banner {
+    display: none;
+  }
+  .instruction .btn {
+    width: 12.5em;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  /* .brand {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  } */
+  .brand {
+    text-align: left !important;
+    justify-content: flex-start;
+  }
+  .btns-wrapper {
+    padding-top: 0px;
+    margin-bottom: 10px;
+  }
+
+  .mt-2 .editor-btn{
+    max-width: 100%;
+    flex: 0 0 100%;
+  }
+
+  .labels-card {
+    max-height: 100%;
+    height: 100%;
+  }
+
+  .btns {
+    position: absolute;
+    top: -5.7em;
+    left: 0;
+    width: 100%;
+  }
+
+  .btns-wrapper .btn {
+    width: 120px;
+  }
+
+  .list-group {
+    width: 80%;
+    margin: auto;
+  }
+
+  .list-group {
+    flex-direction: row;
+  }
+  .list-group-item, .list-group-item:nth-child(2) {
+    width: 150px;
+  }
+  #stageparent {
+    width: 100% !important;
+    height: 21em !important;
+    padding-top: 0.5em;
+  }
+  #container {
+    width: 90% !important;
+    height: 18em !important;
+  }
+  #labels {
+    padding: 0px 15px;
+  }
+  .canvas-btns {
+    bottom: 0;
+  }
+  .properties-wrapper h5 {
+    display: none;
+  }
+  .slidecontainer p,
+  .slidecontainer input {
+    margin: 0px;
+  }
+  .download-btn {
+    position: absolute;
+    left: calc(50% - 68px);
+  }
+  /* .canvas {
+    margin-top: 10px;
+  } */
+  .right-panel {
+    margin-top: 20px;
+  }
+  .brand h1 {
+    font-size: 1.6em;
+    margin-left: 10px;
+  }
+
+  .bottomright {
+    width: 45%;
+    right: 0;
+    bottom: 0;
+  }
+
+  .bottomleft {
+    width: 45%;
+  }
+
+  .card-body {
+    padding: 0.4rem; }
+
+}
+
+@media screen and (max-device-width: 480px) {
+  .header{
+    padding: 15px 0px 0px;
+  }
+
+  .top-logo{
+    margin-bottom: -60px;
+    width: 90px;
+  }
+
+  .top-logo-name{
+    padding-left: 0px!important;
+  }
+
+  .wrapper {
+    margin-top: 5em;
+  }
+
+  #properties {
+    -ms-flex: 0 0 21%;
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+
+  .d-inline{
+      font-size: 1.85em!important;
+      margin-left: 10px;
+  }
+
+  .btns-wrapper .btn {
+    width: 90px;
+    height: 35px;
+    line-height: 20px;
+    /* margin-left: 10px; */
+    color: #fff;
+    margin-top: 0px!important;
+    margin: 10px!important;
+   }
+
+   #labels-container {
+    margin-top: 40px;
+    padding-left: 0px;
+    overflow: auto;
+   }
+
+   .download-btn {
+      position: absolute;
+      top: -105px;
+      left: calc(50% - 68px);
+    }
+
+   #downloads{
+    padding: 5px 10px;
+   }
+
+   .canvas-col{
+    padding-left: 0px;
+    margin-top: 120px;
+   }
+
+   .list-group .btn{
+    padding: 0px;
+   }
+
+   .closeL{
+    font-size: 14px!important;
+   }
+
+   .closeL img{
+    display: none;
+   }
+
+   .elementlabel{
+    font-size: 14px;
+    padding: 5px 1px;
+   }
+
+   .move-up-element, .move-down-element{
+    font-size: 13px;
+   }
+
+   .list-group{
+    width: 80%;
+    margin: 0 auto;
+   }
+
+   .list-group-item {
+     width: 90px;
+    }
+
+    .list-group-item, .list-group-item:nth-child(2) {
+      width: 70px;
+    }
+
+   .editor-btn{
+    padding: 5px;
+    flex: 0 0 100%;
+    max-width: 100%;
+   }
+
+   .editor-btn-lt {
+      flex: 0 0 100%;
+      max-width: 100%;
+   }
+
+   .btns {
+      left: 0;
+      width: 100%!important;
+    }
+
+   .icon-btn {
+      display: block; 
+    }
+
+    .right-panel {
+        margin-top: 20px;
+        margin-bottom: 70px;
+    }
+
+    .labels-card {
+      max-height: 30.7em;    
+    }
+
+    #stageparent {
+      width: 100% !important;
+      height: 21.8em !important;
+      padding-top: 1em;
+  }
+}
+
+@media screen and (max-width: 350px) {
+  h1 {
+    font-size: 1.5em !important;
+  }
+  .brand {
+    padding: 0px;
+  }
+  #stageparent {
+    width: 100% !important;
+    height: 16em !important;
+    padding-top: 0.5em;
+  }
+  #container {
+    width: 90% !important;
+    height: 13em !important;
+  }
+  .instruction {
+    position: relative;
+    top: -85px !important;
+  }
+  .instruction .btn {
+    width: 10em !important;
+  }
+}
+</style>
+
 <!-- <div id="measure"></div> -->
 <header class="header">
     <div class="container-fluid" style="margin: 0px; padding: 0px;">
         <div class="row" style="margin: 0px; padding: 0px;">
-            <div class="col-lg-8 col-md-7 col-sm-5 text-center d-flex text-white brand">
-                <img class="d-inline" src="assets/img/Logo.svg" width="120px" />
+            <div class="col-lg-8 col-md-7 col-sm-5 text-center top-logo-name d-flex text-white brand">
+                <img class="d-inline top-logo" src="assets/img/Logo.svg" width="120px" />
                 <h1 class="d-inline">My Stamp Maker</h1>
                 <!-- <a href="pageMaker/index.html">Second Page</a> -->
 
             </div>
-            <div class="col-lg-4 col-md-5 col-sm-6" style="display: flex; align-items:center">
+            <div class="col-lg-4 col-md-5 col-sm-6 header-navbar-buttons">
                 <div class="btns-wrapper">
                     <!-- <p class="mt-4">
                         <a href="pageMaker/" class="text-white" style="text-decoration: none;">Create Page</a>
@@ -27,13 +342,13 @@ include_once('inc/header.php');
                         if (isset($_SESSION["uid"])) {
 
                             $d = $_SESSION["uid"];
-                            echo "<h6 style='margin:20px;'>Welcome, " . $d['username'] . "</h6><button class='btn btn-outline-warning' onclick='logout()' style='height:40px;margin:20px;'>Log Out</button>";
+                            echo "<h6 style='margin:20px; color: #ececec;'>Welcome, " . $d['username'] . "</h6><button class='btn btn-outline-warning' onclick='logout()' style='height:40px;margin:20px;'>Log Out</button>";
                             if ($d['role'] == "admin") {
                                 // echo "<button class='btn btn-outline-warning' onclick='Dashboard()' style='height:40px;margin:20px;'>Dashboard</button>";
                                 echo "<a class='btn btn-outline-warning' href='admin' style='height:40px;margin:20px;'>Dashboard</a>";
                             }
                         } else {
-                            echo "<button type='button' class='btn btn-outline-warning' data-toggle='modal' data-target='#myModal' style='height:40px;margin:20px;'>Sign Up</button><button type='button' class='btn btn-outline-warning' data-toggle='modal' data-target='#myModalLogin' style='height:40px;margin:20px;' onclick='reset()'>Log In</button>";
+                            echo "<button type='button' class='btn btn-outline-warning signup-btn' data-toggle='modal' data-target='#myModal'>Sign Up</button><button type='button' class='btn btn-outline-warning' data-toggle='modal' data-target='#myModalLogin' style='height:40px;margin:20px;' onclick='reset()'>Log In</button>";
                         }
                     }
                     ?>
@@ -49,7 +364,7 @@ include_once('inc/header.php');
             <ul class="list-group">
                 <li class="list-group-item">
 
-                    <button class="btn o icon-btn" id="addroundtext" title="Text Around The Circle">
+                    <a class="btn o icon-btn" id="addroundtext" title="Text Around The Circle">
                         <img src="assets/img/IconRound.svg" alt="">
                         <!-- <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px" height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
                             <path fill="#000000" d="M136 153l-37 0 -10 23 -27 0 49 -102 27 0 27 55c-12,5 -22,13 -29,24zm5 -17l-17 -38 -17 38 34 0zm-21 -126c-61,0 -110,50 -110,111 0,61 49,110 110,110 7,0 13,0 20,-2 -7,-6 -12,-14 -16,-23 -1,0 -2,0 -4,0 -47,0 -85,-38 -85,-85 0,-47 38,-85 85,-85 47,0 85,38 85,85 0,1 0,1 0,2 9,3 18,8 24,15 1,-6 2,-12 2,-17 0,-61 -50,-111 -111,-111zm60 140l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -28 0 -2zm2 -15c-30,4 -50,30 -46,58 3,27 27,50 58,46 16,-2 27,-10 34,-18 19,-23 15,-57 -6,-74 -11,-9 -24,-14 -40,-12z">
@@ -57,10 +372,10 @@ include_once('inc/header.php');
                         </svg> -->
 
                         <span style="color:black; text-align:left">Round Text</span>
-                    </button>
+                    </a>
                 </li>
                 <li class="list-group-item">
-                    <button class="btn o icon-btn" id="addlinetext" title="Line Text">
+                    <a class="btn o icon-btn" id="addlinetext" title="Line Text">
                         <img src="assets/img/IconLine.svg" alt="">
                         <!-- <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px" height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
                             <path fill="#000000" d="M5 236l60 1 15 -38 45 0c-1,-20 3,-35 15,-50 1,-1 1,-2 2,-2l-41 -1 24 -59 22 54c1,0 7,-5 10,-7 13,-8 27,-11 43,-10l-45 -113 -60 0 -90 225zm180 -82l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -27 0 -3zm2 -15c-30,4 -50,30 -47,58 3,27 28,50 58,47 16,-2 28,-10 35,-19 19,-22 15,-56 -6,-74 -11,-8 -25,-14 -40,-12z">
@@ -68,10 +383,10 @@ include_once('inc/header.php');
                         </svg> -->
                         <span style="color:black; text-align:left">Line Text</span>
 
-                    </button>
+                    </a>
                 </li>
                 <li class="list-group-item">
-                    <button class="btn o icon-btn" id="addcircle" title="Circle">
+                    <a class="btn o icon-btn" id="addcircle" title="Circle">
                         <img src="assets/img/IconShape.svg" alt="">
                         <!-- <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px" height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
                             <path fill="#000000" d="M125 15c-61,0 -110,49 -110,110 0,61 49,110 110,110 7,0 13,0 20,-1 -7,-7 -12,-15 -16,-24 -1,0 -3,0 -4,0 -47,0 -85,-38 -85,-85 0,-47 38,-85 85,-85 47,0 85,38 85,85 0,1 0,2 0,2 9,3 17,8 24,15 1,-6 1,-11 1,-17 0,-61 -49,-110 -110,-110zm60 139l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -27 0 -3zm2 -15c-30,4 -50,30 -47,58 3,27 28,50 58,47 16,-2 28,-10 35,-19 19,-22 15,-56 -6,-74 -11,-8 -25,-14 -40,-12z">
@@ -80,10 +395,10 @@ include_once('inc/header.php');
                         <span style="color:black; text-align:left">Circle</span>
 
 
-                    </button>
+                    </a>
                 </li>
                 <li class="list-group-item">
-                    <button class="btn o icon-btn" id="addimage" title="Image">
+                    <a class="btn o icon-btn" id="addimage" title="Image">
                         <img src="assets/img/IconSquare.svg" alt="">
                         <!-- <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 250 250" width="24px" height="24px" version="1.0" xmlns:xlink="http://www.w3.org/1999/xlink">
                             <path class="add_el_path" fill="#000000" d="M185 154l15 0 0 30 30 0 0 15 -30 0 0 30 -15 0 0 -30 -30 0 0 -15 30 0 0 -27 0 -3zm2 -15c-30,4 -50,30 -47,58 3,27 28,50 58,47 16,-2 28,-10 35,-19 19,-22 15,-56 -6,-74 -11,-8 -25,-14 -40,-12zm-83 -25c0,-11 -8,-20 -19,-20 -11,0 -20,9 -20,20 0,11 9,20 20,20 11,0 19,-9 19,-20zm-44 72l0 23 69 0c-2,-7 -4,-14 -4,-21 0,-22 12,-41 29,-53l-9 -12 -20 29 -25 -11 -40 45zm32 -174l-58 61 0 164 114 0c-5,-5 -9,-9 -12,-14l-88 0 0 -144 53 0 0 -53 101 0 0 99c5,1 9,3 14,5l0 -118 -124 0zm-5 26l0 27 -25 0 25 -27z">
@@ -91,7 +406,7 @@ include_once('inc/header.php');
                         </svg> -->
                         <span style="color:black; text-align:left">Image</span>
 
-                    </button>
+                    </a>
                 </li>
 
             </ul>
@@ -122,7 +437,7 @@ include_once('inc/header.php');
             <div class="row">
                 <!-- <ul class="bookmarks col-lg-2 col-sm-2 col-md-3 col-2" id="labels">
         </ul> -->
-                <div class="col-lg-2 col-md-2 left-banner" style="padding:0px; height:21em">
+                <div class="col-lg-2 col-md-2 left-banner" style="padding:0px;">
                     <img src="assets/img/banner.jpg" alt="" style="width:100%; height:100%">
                 </div>
                 <div class="col-lg-8 col-sm-12 col-md-8 col-12 ">
@@ -149,11 +464,11 @@ include_once('inc/header.php');
                     </center>
                     <center class=" mt-2 download-btn">
                         <?php $session = (isset($_SESSION['uid'])) ? $_SESSION['sessionid'] : 'null'; ?>
-                        <button class="btn btn-lg btn-warning shadow" style="color: white" id="downloads" onclick="down('<?php echo $session ?>')">Download</button>
+                        <button class="btn btn-lg btn-warning shadow" style="color: white; border: none;" id="downloads" onclick="down('<?php echo $session ?>')">Download</button>
 
                     </center>
                 </div>
-                <div class="col-lg-2 col-md-2 right-banner" style="padding:0px; height:21em">
+                <div class="col-lg-2 col-md-2 right-banner" style="padding:0px;">
                     <img src="assets/img/banner.jpg" alt="" style="width:100%; height:100%" />
                 </div>
 
@@ -165,7 +480,6 @@ include_once('inc/header.php');
 
     </div>
 </div>
-
 <script>
     // $(document).click(function(){
     // alert($("#colorpicker").spectrum("get").toHex());
@@ -204,4 +518,5 @@ include_once('inc/header.php');
 </div>
 <!-- Footer -->
 <!-- Footer -->
+
 <?php include 'inc/suggestion.php'; ?>
