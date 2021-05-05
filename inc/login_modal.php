65749af7f030b4acdb6b7c6b2534a9375dab16ae
Muhammad Usman <71489255+muhammadusman5@users.noreply.github.com>
2021-05-05 08:30:43 -0700

    Update login_modal.php

--- inc/login_modal.php
+++ inc/login_modal.php
@@ -2,35 +2,34 @@
 <div class="modal fade" id="myModalLogin" role="dialog">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-
-            </div>
+            <h3 class="login-heading">Log In <button type="button" class="close" data-dismiss="modal">&times;</button></h3>
             <div class="modal-body">
                 <form action="javascript:login()" method="post" id="loginForm">
-                    <center>
-                        <h3>Log In</h3><b id="info" style="color:red"></b><br>
-                    </center>
-                    <input type="text" class="signup form-control" id="usernameLogin" value="<?php if (isset($_COOKIE["usernameLogin"])) {
+                    <b id="info" style="color:red"></b><br>
+                    <div class="form-input-row">
+                        <i class="fa fa-user signup-icons"></i>
+                    <input type="text" class="signup login-input form-control" id="usernameLogin" value="<?php if (isset($_COOKIE["usernameLogin"])) {
                                                                                                     echo $_COOKIE["usernameLogin"];
                                                                                                 } ?>" placeholder="Username" required="">
-                    <br>
-                    <input type="password" class="signup form-control" id="passLogin" placeholder="Password" required="" value="<?php if (isset($_COOKIE["passLogin"])) {
-                                                                                                                                    echo $_COOKIE["passLogin"];
+                    </div><br>
+                    <div class="form-input-row">
+                        <i class="fa fa-key signup-icons"></i>
+                    <input type="password" class="signup login-input form-control" id="passLogin" placeholder="Password" required="" value="<?php if (isset($_COOKIE["passLogin"])) {
+                                                                                             echo $_COOKIE["passLogin"];
                                                                                                                                 } ?>">
-                    <br>
+                    </div><br>
 
                     <center>
-                        <h6 style="font-weight:normal;">Not registered yet.<a id="reg" onclick="notauser();">Click
+                        <h6 style="font-weight:normal;color: #22639a;">Not registered yet.<a id="reg" onclick="notauser();">Click
                                 here</a></h6>
                     </center>
                     <div class="field-group">
                         <div><input type="checkbox" name="remember" id="remember" <?php if (isset($_COOKIE["usernameLogin"])) { ?> checked <?php } ?> />
-                            <label for="remember-me">Remember me</label>
+                            <label for="remember-me" style="color: #22639a;">Remember me</label>
                         </div>
                     </div>
                     <center>
-                        <button class="btn btn-md btn-success modals" id="login">Log In</button>
+                        <button class="btn btn-md login-submit-btn btn-success modals" id="login">Log In</button>
                     </center>
                 </form>
 
